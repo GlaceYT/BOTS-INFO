d4c7dbc5305dc852a92c77fc970a4b632e68593b
GlaceYT <96680732+GlaceYT@users.noreply.github.com>
2024-05-13 21:41:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ client.once('ready', () => {
   });
 
   
-  const channelId = '1162713754048409670'; 
+  const channelId = '1175487983915376662'; 
   client.channels.fetch(channelId).then(channel => {
     if (onlineMessage) {
       onlineMessage.delete().catch(error => console.error('Error deleting message:', error));
